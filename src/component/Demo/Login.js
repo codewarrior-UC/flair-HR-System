3dba518700b8e6c14c5ad1c32751036ae6ecda99
utkarsh <82508741+codewarrior-UC@users.noreply.github.com>
2022-06-13 18:32:54 +0530

    update 13/06/22

--- src/component/Demo/Login.js
+++ src/component/Demo/Login.js
@@ -17,13 +17,14 @@ export default class Login extends React.Component {
         AdminService.AdminLogin(ob)
             .then(response => response.json())
             .then(data => {
-                //   console.log(data)
                 if (data.success) {
                     this.setState({ islogin: true })
                     console.log("login success")
                 }
                 else
                     this.setState({ msg: "Invalid email or Password !" })
+                   
+                    
             })
             .catch(err => {
                 console.log(err)
@@ -32,60 +33,67 @@ export default class Login extends React.Component {
     }
     render() {
        if(this.state.islogin)
-       return <Navigate to="/Menu"/>
+       return <Navigate to="/Navbr"/>
        else  
         return (
-          <section className="section contact-me" data-section="section4">
-            <div className="container">
-              <div className="section-heading">
-                <h2>Admin Login</h2>
-                <div className="line-dec"></div>
-              </div>
-              <div className="row">
-                <div className="right-content">
-                  <div className="container">
-                    <form id="contact" onSubmit={this.login}>
-                      <div className="row">
-                        <div className="col-md-6">
-                          <fieldset>
-                            <input
-                              type="text"
-                              className="form-control"
-                              ref={(c) => (this.emailbox = c)}
-                              placeholder="email ..."
-                              required
-                            />
-                          </fieldset>
+          <section className="vh-100" >
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col col-xl-10">
+                    <div className="card">
+                    <div className="row g-0">
+                        <div className="col-md-6 col-lg-5 d-none d-md-block">
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp" width="300px" height="350px"
+                            alt="login form" className="img-fluid"  />
                         </div>
-                        <div className="col-md-6">
-                          <fieldset>
-                            <input
-                              type="password"
-                              className="form-control"
-                              ref={(c) => (this.passbox = c)}
-                              placeholder="Password..."
-                              required
-                            />
-                          </fieldset>
+                        <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                        <div className="card-body p-4 p-lg-5 text-black">
+                            <form id="contact" onSubmit={this.login}>
+                                <div className="d-flex align-items-center mb-3 pb-1">
+                                    <i className="fas fa-cubes fa-2x me-3" ></i>
+                                    <span className="h1 fw-bold mb-0">FLAiR </span>
+                                </div>
+
+                                <h5 className="fw-normal mb-3 pb-3" >Sign into your account</h5>
+
+                                <div className="form-outline mb-4">
+                                    <input
+                                   id="form2Example17"
+                                   type="text"
+                                    className="form-control"
+                                    ref={(c) => (this.emailbox = c)}
+                                    placeholder="email ..."
+                                    required
+                                     />
+                                    <label className="form-label" for="form2Example17">Email address</label>
+                                </div>
+
+                                <div className="form-outline mb-4">
+                                    <input 
+                                     id="form2Example27"
+                                    type="password"
+                                    className="form-control"
+                                    ref={(c) => (this.passbox = c)}
+                                    placeholder="Password..."
+                                    required
+                                    />
+                                    <label className="form-label" for="form2Example27">Password</label>
+                                </div>
+
+                                <div className="pt-1 mb-4">
+                                    <button 
+                                    className="btn btn-dark btn-lg btn-block" 
+                                     type="submit"
+                                     id="form-submit" >
+                                  Login</button>
+                                </div>
+                            </form>
                         </div>
-                        <div className="col-md-12">
-                          <fieldset>
-                            <button
-                              type="submit"
-                              id="form-submit"
-                              className="button"
-                            >
-                              Login
-                            </button>
-                            &nbsp;
-                            <b classNameName="text-danger">{this.state.msg}</b>
-                          </fieldset>
                         </div>
-                      </div>
-                    </form>
-                  </div>
+                    </div>
+                    </div>
                 </div>
-              </div>
+                </div>
             </div>
           </section>
         );
